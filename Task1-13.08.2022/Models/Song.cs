fd5cb8be331c7e1503fa252f9c9aa3933ca06096
Ibrahim Aliyev <ibrahimma@code.edu.az>
2022-08-14 22:16:28 +0400

    Task1

---
+++ Task1-13.08.2022/Models/Song.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1_13._08._2022.Models
+{
+    class Song:Program
+    {
+        private string _name;
+        private string _genre;
+        private Singer _singer;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value.Length <= 100) _name = value;
+            }
+        }
+        public string Genre
+        {
+            get
+            {
+                return _genre;
+            }
+            set
+            {
+                if (value == "Pop" || value == "Rock" || value == "Jazz" || value == "Techno") _genre = value;
+            }
+        }
+        public Singer Singer { get; set; }
+        public float Rating;
+        float sum = 0, AverageRating, cnt = 1;
+        public void AddRating()
+        {
+            Rating = Convert.ToSingle(Console.ReadLine());
+            sum = sum + Rating;
+            AverageRating = sum / cnt;
+            cnt++;
+            
+        }
+        public float GetAverageRating()
+        {
+            return AverageRating;
+        }
+    }
+}
---
+++ Task1-13.08.2022/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Task1_13._08._2022.Models;
+
+namespace Task1_13._08._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Singer FrankSinatra = new Singer { Name = "Frank", Surname = "Sinatra", Age = 87 };
+            Song song = new Song
+            {
+                Name = "Fly Me To The Moon",
+                Genre = "Rock",
+                Singer = FrankSinatra,
+            };
+
+            int value;
+            do
+            {
+                Console.Write("Enter the rating that you give to the song:");
+                song.AddRating();
+                Console.WriteLine($"Name:{song.Name}\nGenre:{song.Genre}\nSinger:{song.Singer.Name} {song.Singer.Surname} {song.Singer.Age}\nRating:{song.GetAverageRating()}\n");
+                Console.WriteLine("If you want to exit, press 0");
+                Console.WriteLine("If you want to continue, press 1");
+                value = Convert.ToInt32(Console.ReadLine());
+            } while (value != 0);
+            
+        }
+    }
+}
